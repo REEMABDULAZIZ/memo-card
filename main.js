480f035c259966ab89e6b3145284b8e525c2fdf2
muunakh <mmuuna8000@gmail.com>
2019-04-18 12:46:29 +0300

    Final project

---
+++ main.js
@@ -0,0 +1,27 @@
+
+function box1(){
+    document.getElementById('job-title').innerHTML = 'I am a Doctor';
+    document.getElementById('job-desc').innerHTML = 'I am always care about your health';  
+
+}
+
+function box2(){
+    document.getElementById('job-title').innerHTML = 'I am a fireman';
+    document.getElementById('job-desc').innerHTML = 'I am turn off the fire';  
+
+}
+function box3(){
+    document.getElementById('job-title').innerHTML = 'I am a policeman';
+    document.getElementById('job-desc').innerHTML = 'I am always care about your safety';  
+
+}
+function box4(){
+    document.getElementById('job-title').innerHTML = 'I am a lawyer ';
+    document.getElementById('job-desc').innerHTML = 'I am always help you in your trouble';  
+}
+//
+// function box5(){
+//     document.getElementById('job-title').innerHTML = 'I am a';
+//     document.getElementById('job-desc').innerHTML = 'I am always care about your health';  
+
+// }
